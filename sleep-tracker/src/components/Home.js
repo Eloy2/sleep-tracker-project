f1a61744cb7c213f58716302fb48fd053cd1acb7
Eloy <eloydgutierrez@gmail.com>
2020-05-28 15:10:58 -0400

    added redirect to login incase refresh on home page

--- sleep-tracker/src/components/Home.js
+++ sleep-tracker/src/components/Home.js
@@ -7,6 +7,7 @@ import RecommendedSleep from './RecommendedSleep';
 import Entries from './Entries';
 import axios from 'axios';
 import styled from 'styled-components';
+import { Redirect } from 'react-router-dom';
 
 /*
     username: johndoe1
@@ -35,28 +36,32 @@ function Home(props) {
         });
     }
 
-    return (
-        
-        <Div>
-            <Navigation/>
-            {/* <button onClick={props.getUsers}>getUsers</button> */}
-            {/* <button onClick={() => sendToLog({ username: "johndoe1", password: "123" })}>Login</button> */}
-            {/* <button onClick={() => props.register({ username: "guyperson", password: "123", name: "guy person", age: 20 })}>Register</button> */}
-            {/* <button onClick={() => props.getUserEntries(1)}>getUserEntries</button> */}
-            {/* <button onClick={() => props.getEntry(1, 1)}>getEntry</button> */}
-            {/* <button onClick={() => props.createEntry(1, {date: "5-1-2020", sleep_start: "12am", sleep_end: "2am", total_time: 7, mood_score: 4})}>createEntry</button> */}
-            {/* <button onClick={() => props.editEntry(1, 1, {date: "05-21-2020", sleep_start: "10pm", sleep_end: "6am", total_time: 1, mood_score: 4})}>editEntry</button> */}
-            {/* <button onClick={() => props.deleteEntry(1, 1)}>deleteEntry</button> */}
-            <Graph/>
-            <Entries/>
-            <RecommendedSleep/>
-        </Div>  
-        
-    )
+    if (props.name === null) {
+        return <Redirect to="/" />
+    } else {
+        return (
+            
+            <Div>
+                <Navigation/>
+                {/* <button onClick={props.getUsers}>getUsers</button> */}
+                {/* <button onClick={() => sendToLog({ username: "johndoe1", password: "123" })}>Login</button> */}
+                {/* <button onClick={() => props.register({ username: "guyperson", password: "123", name: "guy person", age: 20 })}>Register</button> */}
+                {/* <button onClick={() => props.getUserEntries(1)}>getUserEntries</button> */}
+                {/* <button onClick={() => props.getEntry(1, 1)}>getEntry</button> */}
+                {/* <button onClick={() => props.createEntry(1, {date: "5-1-2020", sleep_start: "12am", sleep_end: "2am", total_time: 7, mood_score: 4})}>createEntry</button> */}
+                {/* <button onClick={() => props.editEntry(1, 1, {date: "05-21-2020", sleep_start: "10pm", sleep_end: "6am", total_time: 1, mood_score: 4})}>editEntry</button> */}
+                {/* <button onClick={() => props.deleteEntry(1, 1)}>deleteEntry</button> */}
+                <Graph/>
+                <Entries/>
+                <RecommendedSleep/>
+            </Div>  
+            
+        )
+    }
 }
 
 export default connect((state) => {
     return {
-        users: state.users
+        name: state.name
     }
 }, { getUsers: getUsers, login: login, getUserEntries: getUserEntries, register: register, getEntry: getEntry, createEntry: createEntry, editEntry: editEntry, deleteEntry: deleteEntry })(Home);
